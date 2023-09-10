fa5c7298249523a01556fd2cfd51436777495ffa
demo <demo@gmail.com>
2023-09-10 23:17:17 +0530

    commits

--- api/maven-api-model/src/main/java/org/apache/maven/api/model/package-info.java
+++
@@ -1,6 +0,0 @@
-// CHECKSTYLE_OFF: RegexpHeader
-/**
- * Maven Immutable POM (Project Object Model) classes, generated from <code>maven.mdo</code> model.
- * The root class is {@link org.apache.maven.api.model.Model}.
- */
-package org.apache.maven.api.model;
